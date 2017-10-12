9744586c659662bc07943ba381c39f39b5e8e910
Barbuseries <francois.edorh@isen.yncrea.fr>
2017-10-12 18:25:39 +0200

    hello, world + Leader - Follower classes (Q 1, 2 and 3)

---
+++ SingleFileBehavior.cpp
@@ -0,0 +1,73 @@
+#include "SingleFileBehavior.h"
+#include "ParamLoader.h"
+#include "SteeringBehaviors.h"
+
+// TODO: Maybe add an entity type in BaseGameEntity, and check it in
+// Update of Follower.
+Leader::Leader(GameWorld* world, Vector2D& initialPos)  : m_IsMindControlled(false),
+														  Vehicle(world,
+																  initialPos,                 //initial position
+																  RandFloat()*TwoPi,        //start rotation
+																  Vector2D(0, 0),            //velocity
+																  Prm.VehicleMass,          //mass
+																  Prm.MaxSteeringForce,     //max force
+																  Prm.MaxSpeed,             //max velocity
+																  Prm.MaxTurnRatePerSecond, //max turn rate
+																  Prm.VehicleScale * 5)
+{
+	Steering()->WanderOn();
+}
+
+Follower::Follower(GameWorld* world, Vector2D& initialPos) : Vehicle(world,
+																	 initialPos,                 //initial position
+																	 RandFloat()*TwoPi,        //start rotation
+																	 Vector2D(0, 0),            //velocity
+																	 Prm.VehicleMass,          //mass
+																	 Prm.MaxSteeringForce,     //max force
+																	 Prm.MaxSpeed,             //max velocity
+																	 Prm.MaxTurnRatePerSecond, //max turn rate
+																	 Prm.VehicleScale)        //scale
+{
+	static bool isFirst = true;
+
+	m_IsFollowingLeader = isFirst;
+	isFirst = false;
+
+	if (!m_IsFollowingLeader) {
+		Vehicle* target = World()->Agents().back();
+		SetTarget(target);
+	}
+}
+
+void Follower::SetTarget(Vehicle* target) {
+	if (target) {
+		Steering()->OffsetPursuitOn(target, Vector2D(0.5, 0.5));
+	}
+}
+
+void Follower::Update(double time_elapsed) {
+	if (m_IsFollowingLeader) {
+		auto& allAgents = World()->Agents();
+		double minDistanceSq = -1;
+		Vehicle *leader = NULL;
+
+		for (int i = 0; i < Prm.NumLeaders; ++i) {
+			Vehicle *agent = allAgents[i];
+
+			// Make sure we are actually looking at a Leader agent.
+			assert(dynamic_cast<Leader *>(agent));
+				
+			double distanceSq = (agent->Pos() - Pos()).LengthSq();
+			
+			if ((minDistanceSq == -1) ||
+				(distanceSq < minDistanceSq)) {
+				leader = agent;
+				minDistanceSq = distanceSq;
+			}
+		}
+
+		SetTarget(leader);
+	}
+
+	Vehicle::Update(time_elapsed);
+}
---
+++ SingleFileBehavior.h
@@ -0,0 +1,32 @@
+#ifndef _SINGLEFILEBEHAVIOR_H_
+#define _SINGLEFILEBEHAVIOR_H_
+
+#include "Vehicle.h"
+#include "GameWorld.h"
+
+// NOTE: This implements Leader - Follower behaviour
+//
+// The follower is supposed to 'follow the agent from its group just
+// in front of him'. This is implemented as 'follow the previously
+// created agent'.
+class Leader : public Vehicle
+{
+private:
+	bool m_IsMindControlled;
+	
+public:
+    Leader(GameWorld* world, Vector2D& initialPos);
+};
+
+
+class Follower : public Vehicle
+{
+private:
+	bool m_IsFollowingLeader;
+	void SetTarget(Vehicle* target);
+	
+public:
+    Follower(GameWorld* world, Vector2D& initialPos);
+	virtual void Update(double time_elapsed);
+};
+#endif /* _SINGLEFILEBEHAVIOR_H_ */
